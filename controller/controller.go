c9bf9a09f7488c133d4e6c033a4c6220adbb0ae7
andrewnguyen22 <andrewnguyen22@protonmail.com>
2025-08-28 10:47:15 -0400

    try remove safety condition

--- controller/controller.go
+++ controller/controller.go
@@ -259,15 +259,15 @@ func (c *Controller) LoadMinimumEvidenceHeight(rootChainId, rootHeight uint64) (
 }
 
 // NextBFTCommitTime() returns the estimated next CommitTime()
-func (c *Controller) NextBFTCommitTime(height uint64) uint64 {
+func (c *Controller) NextBFTCommitTime() uint64 {
 	blockTime := time.Duration(c.Config.BlockTimeMS()) * time.Millisecond
 	nextBFTStartTime := c.Consensus.LastCommitTime.Add(blockTime)
 	c.log.Infof("Next BFT commit time: %s", nextBFTStartTime.Format("15:04:05"))
-	if nextBFTStartTime.Add(-1*blockTime).Before(time.Now()) || time.Until(nextBFTStartTime) > blockTime {
-		c.log.Warnf("Next BFT commit time too early (%s), using time.Now(%s)", nextBFTStartTime.Format("15:04:05"),
-			nextBFTStartTime.Format("15:04:05"))
-		return uint64(time.Now().UnixMicro())
-	}
+	//if nextBFTStartTime.Add(-1*blockTime).Before(time.Now()) || time.Until(nextBFTStartTime) > blockTime {
+	//	c.log.Warnf("Next BFT commit time too early (%s), using time.Now(%s)", nextBFTStartTime.Format("15:04:05"),
+	//		time.Now().Format("15:04:05"))
+	//	return uint64(time.Now().UnixMicro())
+	//}
 	return uint64(nextBFTStartTime.UnixMicro())
 }
 
