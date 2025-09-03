fc3a8152d2d8f2dfa804dae0f8ff6a013da4b2fb
Roniel Valdez <roniel_valdez@outlook.com>
2025-09-03 17:07:48 -0400

    feat: stream test

--- store/badger_test.go
+++ store/badger_test.go
@@ -1,14 +1,98 @@
 package store
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"log"
 	"testing"
 	"time"
 
 	"github.com/canopy-network/canopy/fsm"
 	"github.com/canopy-network/canopy/lib"
 	"github.com/canopy-network/canopy/lib/crypto"
+	"github.com/dgraph-io/badger/v4"
+	"github.com/dgraph-io/ristretto/v2/z"
 )
+
+func TestStoreStream(t *testing.T) {
+	config := lib.Config{
+		StoreConfig: lib.StoreConfig{
+			CleanupBlockInterval: 200,
+			InMemory:             false,
+		},
+	}
+	db, err := NewStore(config, "canopy", nil, lib.NewDefaultLogger())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	readOnlyStore, err := db.NewReadOnly(db.Version() - 2)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	chainId := uint64(1)
+
+	stream := db.DB().NewStreamAt(db.Version() - 2)
+	stream.NumGo = 2
+	prefix := lib.Append([]byte(historicStatePrefix), fsm.CommitteePrefix(chainId))
+	stream.Prefix = prefix
+
+	stream.ChooseKey = func(item *badger.Item) bool {
+		return true
+	}
+
+	count := 0
+	stream.KeyToList = nil
+	stream.Send = func(buf *z.Buffer) error {
+		count++
+		kvList, err := badger.BufferToKVList(buf)
+		if err != nil {
+			return err
+		}
+
+		for i, kv := range kvList.GetKv() {
+			cleaned := bytes.TrimPrefix(kv.GetKey(), prefix)
+			address, err := fsm.AddressFromKey(cleaned)
+			if err != nil {
+				return err
+			}
+			// load the validator from the state using the address
+			_, err = GetValidator(readOnlyStore, address)
+			if err != nil {
+				return err
+			}
+			count++
+			fmt.Printf("Validator [%d] %s\n", i, address.String())
+		}
+
+		return nil
+	}
+
+	start := time.Now()
+	if err := stream.Orchestrate(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("[0] Count: %d Elapsed: %s\n", count, time.Since(start))
+
+	// MaxCommitteeSize := uint64(1_000_000_000)
+	// i := uint64(0)
+	// for ; it.Valid() && i < MaxCommitteeSize; func() { it.Next(); i++ }() {
+	// 	address, e := fsm.AddressFromKey(it.Key())
+	// 	if e != nil {
+	// 		t.Fatal(e)
+	// 	}
+	// 	// load the validator from the state using the address
+	// 	_, e = GetValidator(readOnlyStore, address)
+	// 	if e != nil {
+	// 		t.Fatal(e)
+	// 	}
+	// }
+	// it.Close()
+	// readOnlyStore.Discard()
+	// }
+}
 
 func TestStore(t *testing.T) {
 	config := lib.Config{
@@ -38,6 +122,7 @@ func TestStore(t *testing.T) {
 	MaxCommitteeSize := uint64(1_000_000_000)
 	i := uint64(0)
 	for ; it.Valid() && i < MaxCommitteeSize; func() { it.Next(); i++ }() {
+		fmt.Println("Non stream Key:", lib.BytesToString(it.Key()))
 		address, e := fsm.AddressFromKey(it.Key())
 		if e != nil {
 			t.Fatal(e)
