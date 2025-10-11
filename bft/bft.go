06ece3b34dbbdee036f5cfef39f1e55290417624
andrewnguyen22 <andrewnguyen22@protonmail.com>
2025-10-11 14:26:04 -0400

    testnet protocol height

--- bft/bft.go
+++ bft/bft.go
@@ -22,7 +22,7 @@ import (
 //   2) Upgrade height which switches to the embedded 'root build height' once Validators all upgraded
 //   3) Cleanup the deprecated code
 
-var PROTOCOL_BREAK_UPGRADE_HEIGHT = uint64(0)
+var PROTOCOL_BREAK_UPGRADE_HEIGHT = uint64(1200)
 
 func (b *BFT) BeforeUpgradeHeight() bool {
 	if b.LoadIsOwnRoot() {
